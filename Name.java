19bacd0aa04c1a4885372a2a33b38ff1067d76e6
saniyakamath-1007 <saniyakamath29@gmail.com>
2024-12-11 14:20:49 +0530

    done

---
+++ Name.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Name
+{
+public static void main(String args[])
+{
+Scanner myobj=new Scanner(System.in);
+System.out.println("enter username");
+String username;
+username=myobj.nextLine();
+System.out.println("username is "+username);
+}
+}
